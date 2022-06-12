fea6709ba761342af4199cc8942adadfaecbbe29
Marcell Munnich <marcellmunnich@gmail.com>
2022-06-12 20:49:10 +1000

    created base functionality of API

---
+++ ToDo_app/api/views.py
@@ -0,0 +1,62 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
+
+# Create your views here.
+
+
+@api_view(["GET"])
+def apiOverview(request):
+    api_urls = {
+        "List": "/task-list",
+        "Create": "/task-create",
+        "Update": "/task-update/<str:pk>/",
+        "Delete": "/task-delete/<str:pk>/",
+    }
+    return Response(api_urls)
+
+
+@api_view(["GET"])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def taskDetail(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(task, many=False)
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def taskUpdate(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(["DELETE"])
+def taskDelete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+
+    return Response("Item successfully deleted!")
